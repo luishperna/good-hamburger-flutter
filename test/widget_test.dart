1bb90b8012a7e547ce95d52a195ec67a39090415
Luís Perna <luishperna@gmail.com>
2025-11-17 23:03:24 -0300

    refactor: rename project from good_hamgurger to good_hamburger

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:good_hamgurger/main.dart';
+import 'package:good_hamburger/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
